15b45ec0c7661bf60b9eb255096eeefeabcd4346
jtrobbins <jeffrbns@gmail.com>
2026-02-24 00:34:11 -0500

    Added Home Title and Resume Categories

--- composeApp/src/webMain/kotlin/com/jtrobbins/mywebsite/presentation/feature/home/HomeScreen.kt
+++ composeApp/src/webMain/kotlin/com/jtrobbins/mywebsite/presentation/feature/home/HomeScreen.kt
@@ -1,18 +1,38 @@
 package com.jtrobbins.mywebsite.presentation.feature.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.jtrobbins.mywebsite.presentation.common.components.CustomCard
+import com.jtrobbins.mywebsite.presentation.common.components.Footer
 
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
     ) {
-        Text("Home Screen")
+        CustomCard {
+            Text(
+                text = "Full-Stack Software Developer",
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = DividerDefaults.Thickness,
+            color = DividerDefaults.color
+        )
+        Footer()
     }
 }
--- composeApp/src/webMain/kotlin/com/jtrobbins/mywebsite/presentation/feature/resume/ResumeScreen.kt
+++ composeApp/src/webMain/kotlin/com/jtrobbins/mywebsite/presentation/feature/resume/ResumeScreen.kt
@@ -1,18 +1,71 @@
 package com.jtrobbins.mywebsite.presentation.feature.resume
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.jtrobbins.mywebsite.presentation.common.components.Footer
 
 @Composable
 fun ResumeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
     ) {
-        Text("Resume Screen")
+        Text(
+            text = "Education",
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = DividerDefaults.Thickness,
+            color = DividerDefaults.color
+        )
+        Text(
+            text = "Experience",
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = DividerDefaults.Thickness,
+            color = DividerDefaults.color
+        )
+        Text(
+            text = "Publications",
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = DividerDefaults.Thickness,
+            color = DividerDefaults.color
+        )
+        Text(
+            text = "Skills",
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = DividerDefaults.Thickness,
+            color = DividerDefaults.color
+        )
+        Text(
+            text = "Awards",
+            modifier = Modifier.padding(start = 16.dp)
+        )
+        HorizontalDivider(
+            modifier = Modifier.padding(vertical = 8.dp),
+            thickness = DividerDefaults.Thickness,
+            color = DividerDefaults.color
+        )
+        Footer()
     }
 }
