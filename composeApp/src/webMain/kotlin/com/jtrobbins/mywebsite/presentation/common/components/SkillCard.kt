7d1b89bacdbfccd1486967a010840c95323e56e9
jtrobbins <jeffrbns@gmail.com>
2026-02-24 23:58:31 -0500

    Fixed Skill Card Scrolling

--- composeApp/src/webMain/kotlin/com/jtrobbins/mywebsite/presentation/common/components/SkillCard.kt
+++ composeApp/src/webMain/kotlin/com/jtrobbins/mywebsite/presentation/common/components/SkillCard.kt
@@ -1,10 +1,11 @@
 package com.jtrobbins.mywebsite.presentation.common.components
 
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,11 +17,13 @@ fun  SkillCard(
     category: String,
     skills: List<String>
 ) {
+    val scrollState = rememberScrollState()
     CustomCard {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 16.dp),
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+                .horizontalScroll(scrollState),
             verticalAlignment = Alignment.CenterVertically,
         ) {
             Text(
